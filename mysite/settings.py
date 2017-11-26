e0848abf0cfd9effeac8c301c6d6eee19a5a49c4
Joseph Sayegh <sayegh_j@hotmail.com>
2017-11-26 23:32:15 +0100

    DIRS edited

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Django\blog\template\blog'],
+        'DIRS': ['C:\Users\sayeg\Documents\Centrale\3A\Django\blog\template\blog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
